6d10aa1747412f10aab9895d5a1fc21622e6904f
LaTricia Morris <latricia@thebrandrevivalist.com>
2026-07-28 17:53:50 +0000

    File changes

--- src/components/ResetIcon.jsx
+++ src/components/ResetIcon.jsx
@@ -4,25 +4,12 @@
  */
 export default function ResetIcon({ className = 'w-4 h-4', style, ...props }) {
   return (
-    <svg
+    <img
+      src="https://media.base44.com/images/public/6a312911bcddb0806c388af8/74832b145_transparentbackgroundResetIcon.svg"
+      alt="Reset"
       className={className}
-      style={{ flexShrink: 0, display: 'block', ...style }}
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="1.8"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      style={{ flexShrink: 0, display: 'block', objectFit: 'contain', ...style }}
       {...props}
-    >
-      {/* Top arrow: starts at top center, curves clockwise, arrowhead top-left */}
-      <path d="M12 3 a9 9 0 0 1 6.5 2.7" />
-      <path d="M18.5 2v5h-5" />
-      {/* Bottom arrow: starts at bottom center, curves counter-clockwise, arrowhead bottom-right */}
-      <path d="M12 21 a9 9 0 0 1 -6.5 -2.7" />
-      <path d="M5.5 22v-5h5" />
-      {/* Inner circle */}
-      <circle cx="12" cy="12" r="3.5" />
-    </svg>
+    />
   );
 }
